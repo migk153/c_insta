3a2e834c9d7aea1025fe2df55dfef93526ed2230
migk153 <migk153@gmail.com>
2019-01-29 13:39:47 +0900

    add like and comments. also add FKs

--- c_insta/images/models.py
+++ c_insta/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from c_insta.users import models as user_models
 
 # Create your models here.
 
@@ -10,17 +11,26 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-
-
 class Image(TimeStampedModel):
+
+    """ Image Model """
 
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
-
-
-
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
 
 class Comment(TimeStampedModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
+
+class Like(TimeoutError):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
