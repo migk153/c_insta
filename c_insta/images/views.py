9c67391e335b464d75ec1fde286163e81ff73c2b
migk153 <migk153@gmail.com>
2019-01-31 17:48:57 +0900

    adding views using serializers

--- c_insta/images/views.py
+++ c_insta/images/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render       We use render when we want to use templates.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models, serializers
+
 
 # Create your views here.
+
+class ListAllImages(APIView):
+
+    def get(self, request, format=None):  # it will only be called when there is a get call over http.
+
+        all_images = models.Image.objects.all()
+
+        serializer = serializers.ImageSerializer(all_images, many=True)
+
+        return Response(data=serializer.data)
