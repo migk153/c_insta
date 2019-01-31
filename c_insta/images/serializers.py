823519627c2d67e18948b948785a029866bf150f
migk153 <migk153@gmail.com>
2019-02-01 00:44:04 +0900

    change field name of __set to common names

--- c_insta/images/serializers.py
+++ c_insta/images/serializers.py
@@ -17,8 +17,8 @@ class LikeSerializer(serializers.ModelSerializer):
 
 class ImageSerializer(serializers.ModelSerializer):
     
-    comment_set = CommentSerializer(many=True)
-    like_set = LikeSerializer(many=True)
+    comments = CommentSerializer(many=True)
+    likes = LikeSerializer(many=True)
 
     class Meta:
         model = models.Image
@@ -27,7 +27,7 @@ class ImageSerializer(serializers.ModelSerializer):
             'file',
             'location',
             'caption',
-            'comment_set',
-            'like_set',
+            'comments',
+            'likes',
         )
 
