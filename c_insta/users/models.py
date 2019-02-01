ecbbb752ecdbf3133d2aa81363a64b1377a35332
migk153 <migk153@gmail.com>
2019-02-01 21:20:59 +0900

    add profile image to users

--- c_insta/users/models.py
+++ c_insta/users/models.py
@@ -19,6 +19,8 @@ class User(AbstractUser):
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
 
+    profile_image = models.ImageField(null=True)
+    name = models.CharField(_('Name of User'), blank=True, max_length=255)
     website = models.URLField(null=True)
     bio = models.TextField(null=True)
     phone = models.CharField(max_length=140, null=True)
