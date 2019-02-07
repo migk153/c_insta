dd8c94e34181ece616b0c3b04fcbe58474db0b6b
migk153 <migk153@gmail.com>
2019-02-07 20:48:13 +0900

    delete my comments

--- c_insta/images/urls.py
+++ c_insta/images/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
     path("<int:image_id>/likes", view=views.LikeImage.as_view(), name="like_image"),
     path("<int:image_id>/comments", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("comments/<int:comment_id>", view=views.Comment.as_view(), name="comment"),
 ]
 
 # /images/3/like
--- c_insta/images/views.py
+++ c_insta/images/views.py
@@ -83,3 +83,17 @@ class CommentOnImage(APIView):
 
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Comment(APIView):
+
+    def delete(self, request, comment_id, format=None):
+
+        user = request.user
+
+        try:
+            found_comment = models.Comment.objects.get(id=comment_id, creator=user)
+            found_comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
