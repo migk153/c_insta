5ff309401b9fbee6c1c52fa30129f6e5477397be
migk153 <migk153@gmail.com>
2019-02-12 23:53:00 +0900

    make work query params and the final form of searching hashtags

--- c_insta/images/urls.py
+++ c_insta/images/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("<int:image_id>/unlike/", view=views.UnLikeImage.as_view(), name="unlike_image"),
     path("<int:image_id>/comments/", view=views.CommentOnImage.as_view(), name="comment_image"),
     path("comments/<int:comment_id>/", view=views.Comment.as_view(), name="comment"),
+    path("search/", view=views.Search.as_view(), name="search"),
 ]
 
 # /images/3/like
