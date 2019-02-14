a7d6ff126a69e8ee163b4567b397ad5546c99bc4
migk153 <migk153@gmail.com>
2019-02-14 17:57:47 +0900

    making fundamental of notifications

--- c_insta/notifications/views.py
+++ c_insta/notifications/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render   We use render when we want to use templates.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . import models, serializers
 
 # Create your views here.
+
+class Notifications(APIView):
+
+    def get(self, request, format=None):
+
+        user = request.user
+
+        notifications = models.Notification.objects.filter(to=user)
+
+        serializer = serializers.NotificationSerializer(notifications, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,10 @@ urlpatterns = [
         "images/",
         include("c_insta.images.urls", namespace="images"),
     ),
+    path(
+        "notifications/",
+        include("c_insta.notifications.urls", namespace="notifications"),
+    ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(
