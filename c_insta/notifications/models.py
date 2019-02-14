015c1a784b2e2259c8d87d8314b2b02946d42c56
migk153 <migk153@gmail.com>
2019-02-14 16:37:45 +0900

    created to basics of notifications

--- c_insta/notifications/models.py
+++ c_insta/notifications/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from c_insta.users import models as user_models
+from c_insta.images import models as image_models
 
 # Create your models here.
+# Below it is Just an example
+
+# creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE, related_name='images')
+
+class Notification(image_models.TimeStampedModel):
+
+    TYPE_CHOICES = (
+        # the first place is for the admin panel, the second is for the database
+
+        ('like', 'Like'),
+        ('comment', 'Comment'),
+        ('follow', 'Follow')
+    )
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name='creator')
+    to = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name='to')
+    notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    image = models.ForeignKey(image_models.Image, on_delete=models.CASCADE, null=True, blank=True)
+
+
+
+
+
