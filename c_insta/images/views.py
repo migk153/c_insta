edb905161b4efb03d89b3420c3392adcb6edbff2
migk153 <migk153@gmail.com>
2019-02-15 20:07:31 +0900

    getting users of likes in an image

--- c_insta/images/views.py
+++ c_insta/images/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from c_insta.users import models as user_models
+from c_insta.users import serializers as user_serializers
 from c_insta.notifications import views as notification_views
 
 
@@ -40,6 +42,18 @@ class Feed(APIView):
 
 
 class LikeImage(APIView):
+
+    def get(self, request, image_id, format=None):
+
+        likes = models.Like.objects.filter(image__id=image_id)
+
+        like_creator_ids = likes.values('creator_id')
+
+        users_like = user_models.User.objects.filter(id__in=like_creator_ids)
+
+        serializer = user_serializers.ListUserSerializer(users_like, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, image_id, format=None):
 
